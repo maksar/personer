5b2cae90fd8c25e5d487b2fc81da44fd11d36185
Shestakov, Aleksandr <a.shestakov@itransition.com>
2021-04-11 10:02:19 +0300

    ci

--- ci.nix
+++ ci.nix
@@ -1,3 +1,21 @@
-{
-  personer = (builtins.getFlake "github:maksar/personer").defaultPackage.x86_64-linux;
+builtins.mapAttrs (k: _v:
+  let
+
+    url = "https://github.com/NixOS/nixpkgs/archive/2255f292063ccbe184ff8f9b35ce475c04d5ae69.tar.gz";
+    pkgs = import (builtins.fetchTarball url) { system = k; };
+  in
+  pkgs.recurseIntoAttrs {
+    # These two attributes will appear in your job for each platform.
+    personer = (builtins.getFlake "github:maksar/personer").defaultPackage.${pkgs.system};
+    hello = pkgs.hello;
+    cow-hello = pkgs.runCommand "cow-hello" {
+      buildInputs = [ pkgs.hello pkgs.cowsay ];
+    } ''
+      hello | cowsay > $out
+    '';
+  }
+) {
+  x86_64-linux = {};
+  # Uncomment to test build on macOS too
+  # x86_64-darwin = {};
 }
